756c97d46d390cb6657ce29d815733b5fe390033
Nazareno <nazareno7870@gmail.com>
2022-05-25 12:04:25 -0300

    minimizer config, dev and prod configs

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,25 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/'
+        filename: 'bundle.js'
     },
+    mode: 'development',
     resolve: {
-        extensions: ['.js', '.jsx'],
-        alias: {
-            '@components': path.resolve(__dirname, 'src/components'),
-            '@styles': path.resolve(__dirname, 'src/styles'),
-        }
+        extensions: ['.js', '.jsx']
     },
-    mode: 'production',
     module: {
         rules: [
             {
@@ -39,7 +31,7 @@ module.exports = {
             },
             {
                 test: /\.s[ac]ss$/,
-                use: [
+                use:[
                     'style-loader',
                     'css-loader',
                     'sass-loader'
@@ -54,14 +46,12 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css',
-        }),
-        new CleanWebpackPlugin(),
+        })
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin()
-        ]
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3006
     }
+
 }
